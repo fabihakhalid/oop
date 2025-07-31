d462dd640c03870e8e8028914a3306d82212e60a
fabihakhalid <fabihakhalid2005@gmail.com>
2025-07-30 19:29:51 -0700

    done

---
+++ main.java/src/VariablesDataTypes.java
@@ -0,0 +1,11 @@
+public class VariablesDataTypes {
+
+    public static void main(String[] args) {
+        
+        System.out.println("*******VariavlesDataTypes******");
+
+        int age ;
+        age = 10;
+         System.out.println(age);
+    }
+}
